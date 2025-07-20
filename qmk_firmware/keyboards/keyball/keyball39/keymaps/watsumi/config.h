14ee3531d8d3bbf7c11531df30556466e2f260ef
Wataru Utsumi <67275879+watsumi@users.noreply.github.com>
2025-07-20 22:55:16 +0900

    create new settings

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/watsumi/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/watsumi/config.h
@@ -1,7 +1,7 @@
 /*
 This is the c configuration file for the keymap
 
-Copyright 2021 @Yowkees
+Copyright 2022 @Yowkees
 Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
 
 This program is free software: you can redistribute it and/or modify
@@ -27,16 +27,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
-#undef PRODUCT_ID
-#define PRODUCT_ID 0x0002
+#define OLED_TIMEOUT 30000
+
+#define TAPPING_TERM 180
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#define KEYBALL_CPI_DEFAULT 1100 // マウス速度
+#define KEYBALL_SCROLL_DIV_DEFAULT 5 // スクロール速度
 
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_DEFAULT_LAYER 2
+#define AUTO_MOUSE_TIME 500
